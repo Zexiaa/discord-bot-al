ddbe338d92648c4acad4bba3fe14223c8c556b51
Tan Teck Jun <tanteckjun@gmail.com>
2024-07-09 17:52:34 +0800

    Changed to common postgres setting

--- services/db-util.js
+++ services/db-util.js
@@ -2,21 +2,13 @@ import "dotenv/config";
 import postgres from 'postgres';
 import { pg_database } from '../constants/constants.js';
 
-var sqlConn;
-if (process.env.NODE_ENV === "production") {
-    sqlConn = postgres(process.env.PG_DATABASE_URL)
-}
-else {
-    sqlConn = postgres({
+export const sql = postgres('postgres://username:password@host:port/database', {
         username: process.env.PG_USER,
         password: process.env.PG_PASSWORD,
         host: process.env.PG_HOST,
         port: process.env.PG_PORT,
         database: pg_database
     });
-}
-
-export const sql = sqlConn;
 
 // Create DB and tables
 export const checkDb = async () => {
