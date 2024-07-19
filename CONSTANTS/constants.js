e44382944dee3350f2985d665a6543683bb3c27f
Tan Teck Jun <tanteckjun@gmail.com>
2024-07-19 17:42:50 +0800

    Moved constants file

---
+++ CONSTANTS/constants.js
@@ -0,0 +1,8 @@
+export const db_directory = "./al_database.db"
+
+// Tables
+export const db_reminderTable = 'reminder_message';
+export const db_eventsTable = 'live_events';
+
+// Events
+export const event_reminderTrigger = "remindertrigger";
